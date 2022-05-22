e437524b53305b03c72c3d99d286e0c5e8a02c5a
rida-123 <ridashoaib666@gmail.com>
2022-05-22 15:47:45 -0700

    animation

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -7,47 +7,47 @@ const Skills=()=>{
                 <span className="text-slate-50 text-3xl border-b-4 -mt-[140px] border-[#f7ad94] lg:mt-0">SKILLS</span>
      <span className="mt-5 text-slate-50"> These are the technologies I've worked with</span>
      <div className="grid grid-cols-2 gap-10 mt-8 w-full text-center lg:grid-cols-4">
-               <div className="shadow p-3 hover:scale-110 duration-500">
+               <div className="shadow p-3 hover:scale-110 duration-500 animate-bounce">
                    <img src="html.png" className="h-24 mx-auto"></img>
                    <p className="text-md mt-2 text-slate-50">HTML</p>
                </div>
-               <div className="shadow p-3 hover:scale-110 duration-500">
+               <div className="shadow p-3 hover:scale-110 duration-500 animate-bounce">
                    <img src="css.png" className="h-24 mx-auto"></img>
                    <p className="text-md mt-2 text-slate-50">CSS</p>
                </div>
-               <div className="shadow p-3 hover:scale-110 duration-500">
+               <div className="shadow p-3 hover:scale-110 duration-500 animate-bounce">
                    <img src="javascript.png" className="h-24 mx-auto"></img>
                    <p className="text-md mt-2 text-slate-50">JAVASCRIPT</p>
                </div>
-               <div className="shadow p-3 hover:scale-110 duration-500">
+               <div className="shadow p-3 hover:scale-110 duration-500 animate-bounce">
                    <img src="tailwind.png" className="h-24 mx-auto"></img>
                    <p className="text-md mt-2 text-slate-50">TAILWIND CSS</p>
                </div>
-               <div className="shadow p-3 hover:scale-110 duration-500">
+               <div className="shadow p-3 hover:scale-110 duration-500 animate-bounce">
                    <img src="material-ui.png" className="h-24 mx-auto"></img>
                    <p className="text-md mt-2 text-slate-50">MATERIAL UI</p>
                </div>
-               <div className="shadow p-3 hover:scale-110 duration-500">
+               <div className="shadow p-3 hover:scale-110 duration-500 animate-bounce">
                    <img src="bootst.png" className="h-24 mx-auto"></img>
                    <p className="text-md mt-2 text-slate-50">BOOTSTRAP</p>
                </div>
-               <div className="shadow p-3 hover:scale-110 duration-500">
+               <div className="shadow p-3 hover:scale-110 duration-500 animate-bounce">
                    <img src="react.png" className="h-24 mx-auto"></img>
                    <p className="text-md mt-2 text-slate-50">REACT</p>
                </div>
-               <div className="shadow p-3 hover:scale-110 duration-500">
+               <div className="shadow p-3 hover:scale-110 duration-500 animate-bounce">
                    <img src="node.png" className="h-24 mx-auto"></img>
                    <p className="text-md mt-2 text-slate-50">NODE JS</p>
                </div>
-               <div className="shadow p-3 hover:scale-110 duration-500">
+               <div className="shadow p-3 hover:scale-110 duration-500 animate-bounce">
                    <img src="mongo.png" className="h-24 mx-auto"></img>
                    <p className="text-md mt-2 text-slate-50">MONGO DB</p>
                </div>
-               <div className="shadow p-3 hover:scale-110 duration-500">
+               <div className="shadow p-3 hover:scale-110 duration-500 animate-bounce">
                    <img src="firebase.png" className="h-24 mx-auto"></img>
                    <p className="text-md mt-2 text-slate-50">FIREBASE</p>
                </div>
-               <div className="shadow p-3 hover:scale-110 duration-500">
+               <div className="shadow p-3 hover:scale-110 duration-500 animate-bounce">
                    <img src="github.png" className="h-24 mx-auto"></img>
                    <p className="text-md mt-2 text-slate-50">GITHUB</p>
                </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      animation: {
+        bounce: 'bounce 3s  infinite',
+      }
+    },
   },
   plugins: [],
 }
